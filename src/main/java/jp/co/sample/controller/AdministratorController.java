84dbb35f82afa6bff5f250916251694ebf6fd990
Shiori-Kusaka <shiori.kusaka@rakus-partners.co.jp>
2022-08-02 15:00:46 +0900

    Controller修正

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -3,6 +3,7 @@ package jp.co.sample.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.form.InsertAdministratorForm;
@@ -22,7 +23,7 @@ public class AdministratorController {
 	@Autowired
 	private AdministratorService service;
 	
-	@RequestMapping("")
+	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm(InsertAdministratorForm insertAdministratorForm, Model model) {
 		model.addAttribute("insertAdministratorForm", insertAdministratorForm);
 		return insertAdministratorForm;
