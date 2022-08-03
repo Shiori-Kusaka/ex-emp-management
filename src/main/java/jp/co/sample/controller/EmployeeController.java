92100d9be060be6f992e5dd1d81b9c39c883fd10
Shiori-Kusaka <shiori.kusaka@rakus-partners.co.jp>
2022-08-03 11:15:37 +0900

    EmployeeServiceクラスの従業員詳細ページ表示

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 
@@ -26,6 +28,21 @@ public class EmployeeController {
 	 List<Employee> employeeList = employeeService.showList();
 	 model.addAttribute("employeeList", employeeList);
 	 return "employee/list";
+	}
+	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
+	
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Integer intId = Integer.parseInt(id);
+		Employee employee = employeeService.showDetail(intId);
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 
 }
