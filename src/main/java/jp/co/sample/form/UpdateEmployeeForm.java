af912c9772912dd0960308d26c9f79773f19f8cb
Shiori-Kusaka <shiori.kusaka@rakus-partners.co.jp>
2022-08-03 16:49:43 +0900

    郵便番号と住所の情報更新ビューとバリデーション追加

--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -13,6 +13,9 @@ public class UpdateEmployeeForm {
 	@NotBlank(message = "入力は必須です")
 	@Pattern(regexp = "^[0-9]*$", message ="半角数字で入力してください" )
 	private String dependentsCount;
+	@Pattern(regexp="^[0-9]{3}-^[0-9]{4}$", message = "郵便番号の形式で入力してください(ハイフンあり)")
+	private String zipCode;
+	private String address;
 	public String getId() {
 		return id;
 	}
@@ -25,10 +28,24 @@ public class UpdateEmployeeForm {
 	public void setDependentsCount(String dependentsCount) {
 		this.dependentsCount = dependentsCount;
 	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	@Override
 	public String toString() {
-		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + ", zipCode=" + zipCode
+				+ ", address=" + address + "]";
 	}
+	
 	
 	
 
